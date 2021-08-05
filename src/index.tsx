3557ff87f2f8c7def74fa6699f66107112b2f728
Viktor Zhurbin <vzhurbin@gmail.com>
2021-08-05 22:50:57 +0300

    chore: connect apollo-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
 import { App } from "./App";
 
+const client = new ApolloClient({
+    uri: "https://guided-ferret-24.hasura.app/v1/graphql",
+    cache: new InMemoryCache(),
+    headers: {
+        "x-hasura-admin-secret": "eVsdOYwA85ygQDejX1eojS1A2OLab4d2tZb3aWWXJuQUrpSk7euySuWvCbIuxen5",
+    },
+});
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
