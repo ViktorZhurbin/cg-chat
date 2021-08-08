515fb0ec1bffca2bafff43eaba8fe0d6607314ac
Viktor Zhurbin <vzhurbin@gmail.com>
2021-08-08 20:06:54 +0300

    refctor: move app parts away from index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import { Chat } from "./components/Chat";
+import { ApolloProvider } from "@apollo/client";
+
+import { client } from "@/config/apolloClient";
+import { Chat } from "@/components/Chat";
+
+import { GlobalStyle } from "./globalStyle";
 
 export const App = () => {
-    return <Chat />;
+    return (
+        <ApolloProvider client={client}>
+            <GlobalStyle />
+            <Chat />
+        </ApolloProvider>
+    );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,5 @@
-import { ApolloProvider } from "@apollo/client";
-import React from "react";
 import ReactDOM from "react-dom";
 
-import { client } from "./config/apolloClient";
-
 import { App } from "./App";
-import { GlobalStyle } from "./globalStyle";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <GlobalStyle />
-            <App />
-        </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
